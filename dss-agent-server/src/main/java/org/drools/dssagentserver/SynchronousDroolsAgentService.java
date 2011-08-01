c5ab9ffc95792f1648dc9ec431dd36bc392d577d
salaboy <salaboy@gmail.com>
2011-08-01 20:00:49 -0300

    exposed SynchronousDroolsAgent

--- dss-agent-server/src/main/java/org/drools/dssagentserver/SynchronousDroolsAgentService.java
+++ dss-agent-server/src/main/java/org/drools/dssagentserver/SynchronousDroolsAgentService.java
@@ -5,19 +5,33 @@
 package org.drools.dssagentserver;
 
 import java.util.List;
+import javax.jws.WebParam;
+import javax.jws.WebService;
 import org.drools.fipa.ACLMessage;
 import org.drools.fipa.DroolsAgent;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- *
+ * @author salaboy
  * @author esteban
  */
-public class SyncrhonousDroolsAgent {
 
+@WebService()
+public class SynchronousDroolsAgentService {
+    
     private DroolsAgent agent;
     private SynchronousDroolsAgentResponseInformer responseInformer;
 
-    public List<ACLMessage> tell(ACLMessage message) {
+    public SynchronousDroolsAgentService() {
+        System.out.println("Calling the Constructor!!");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("dss-agents.xml");
+        this.agent =  (DroolsAgent) ctx.getBean("agent1");
+        this.responseInformer = (SynchronousDroolsAgentResponseInformer) ctx.getBean("syncResponseInformer");
+    }
+
+    
+    public List<ACLMessage> tell( ACLMessage message) {
         try {
             agent.tell(message);
         } catch (Throwable t) {
@@ -26,15 +40,15 @@ public class SyncrhonousDroolsAgent {
         return responseInformer.retrieveResponses(message);
     }
 
-    public DroolsAgent getAgent() {
-        return agent;
-    }
-
-    public void setAgent(DroolsAgent agent) {
-        if (!(agent.getResponseInformer() instanceof SynchronousDroolsAgentResponseInformer)) {
-            throw new IllegalArgumentException("Agent's response informer should be of type " + SynchronousDroolsAgentResponseInformer.class.getName() + " and not " + agent.getResponseInformer().getClass().getName());
-        }
-        this.agent = agent;
-        this.responseInformer = (SynchronousDroolsAgentResponseInformer) agent.getResponseInformer();
-    }
+//    public DroolsAgent getAgent() {
+//        return agent;
+//    }
+//
+//    public void setAgent(DroolsAgent agent) {
+//        if (!(agent.getResponseInformer() instanceof SynchronousDroolsAgentResponseInformer)) {
+//            throw new IllegalArgumentException("Agent's response informer should be of type " + SynchronousDroolsAgentResponseInformer.class.getName() + " and not " + agent.getResponseInformer().getClass().getName());
+//        }
+//        this.agent = agent;
+//        this.responseInformer = (SynchronousDroolsAgentResponseInformer) agent.getResponseInformer();
+//    }
 }
