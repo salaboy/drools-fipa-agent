474621b229f4904684722fc9c78527f1d5b27779
salaboy <salaboy@gmail.com>
2011-08-26 17:33:49 -0300

    adding drools-fipa-agent-archetype and moving example to the custom agent example. I've also added the WSDL automatic generation
    For some reason the tests are not working.

--- dss-agent-service/src/main/java/org/drools/dssagentserver/SynchronousDroolsAgentServiceImpl.java
+++ dss-agent-service/src/main/java/org/drools/dssagentserver/SynchronousDroolsAgentServiceImpl.java
@@ -9,6 +9,9 @@ import java.util.List;
 import javax.jws.WebService;
 import javax.xml.bind.annotation.XmlSeeAlso;
 
+
+
+
 import org.drools.fipa.ACLMessage;
 import org.drools.fipa.DroolsAgent;
 import org.drools.fipa.body.acts.Agree;
@@ -26,6 +29,10 @@ import org.drools.fipa.body.content.Ref;
 import org.drools.fipa.body.content.Rule;
 import org.drools.fipa.mappers.MyMapArgsEntryType;
 import org.drools.fipa.mappers.MyMapReferenceEntryType;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -41,31 +48,29 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
                     Ref.class, InformRef.class, Request.class, RequestWhen.class,
                     MyMapReferenceEntryType.class, MyMapArgsEntryType.class})
 public class SynchronousDroolsAgentServiceImpl implements SynchronousDroolsAgentService {
-    
+    private static Logger logger = LoggerFactory.getLogger(SynchronousDroolsAgentServiceImpl.class);
     private DroolsAgent agent;
     private SynchronousDroolsAgentResponseInformer responseInformer;
 
     public SynchronousDroolsAgentServiceImpl() {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("dss-agents.xml");
-        this.agent =  (DroolsAgent) ctx.getBean("agent1");
+        this.agent =  (DroolsAgent) ctx.getBean("agent");
         this.responseInformer = (SynchronousDroolsAgentResponseInformer) ctx.getBean("syncResponseInformer");
     }
 
     
     @Override
-    public List<ACLMessage> tell( ACLMessage message) {
-        System.out.println(">>>>>>>>>>>>>MSG IN = "+message);
+    public List<ACLMessage> tell(ACLMessage message) {
+        logger.info(" >>> IN Message -> "+message.getPerformative().name());
         try {
             agent.tell(message);
         } catch (Throwable t) {
             
-            t.printStackTrace();
         }
         List<ACLMessage> retrieveResponses = responseInformer.retrieveResponses(message);
-        if(retrieveResponses != null){
-            for(ACLMessage msg : retrieveResponses){
-                System.out.println(">>>>>>>>>>>>>MSG OUT = "+msg);
-            }
+        logger.info(" >>> Number of OUT Messages -> "+retrieveResponses.size());
+        for(ACLMessage outMessage : retrieveResponses){
+            logger.info(" >>> OUT Message -> "+outMessage.getPerformative().name());
         }
         return retrieveResponses;
     }
